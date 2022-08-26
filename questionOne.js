ad00453172ca789f130a77a156c7571232517c0f
amir <amirtorkashvand56@gmail.com>
2022-08-26 18:24:38 +0430

    Updated

--- questionOne.js
+++ questionOne.js
@@ -1,7 +1,7 @@
 function findAnagram(str1 , str2){
-    // str1 = str1.toLowerCase();
-    // str2 = str2.toLowerCase();
-    if (str1.length !== str2.length) return
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    if (str1.length !== str2.length)  console.error('character ha tedaad equal nist');
     return str1.split('').sort().join('') === str2.split('').sort().join('')
 }
-console.log(findAnagram('hello','lehol'));
+console.log(findAnagram('hello','lehlol'));
